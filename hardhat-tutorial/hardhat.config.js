1064cb05e69c03a1d49b55f1b26f86b2f0713b52
Diksha soni <dikshasonics@gmail.com>
2022-09-25 16:51:07 +0530

    second commit

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,18 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-
-require("dotenv").config({path: ".env"})
-
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL; //alchemy key gives you access to the rinkeby network
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.10",
+  solidity: "0.8.4",
   networks: {
-    rinkeby: {
-      url: ALCHEMY_API_KEY_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -4,7 +4,7 @@ const {CRYPTO_DEVS_NFT_CONTRACT_ADDRESS} = require("../constants"); // importing
 
 async function main() {
   const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
-  const cryptoDevTokensContract = await ethers.getContractFactory("CryptoDevToken"); //this is the contract that we are providing it some factory libraries to use. In other words it promises us to give it to us
+  const cryptoDevTokensContract = await ethers.getContractFactory("CryptoDevToken"); //this is the contract that we are providing it some factory libraries to use. In other words it promises us to give it to us. In return to promise it provides us with an instance of the contract do that we can call the contract over it
   const deployCryptoDevsToken = await cryptoDevTokensContract.deploy(cryptoDevsNFTContract); // this promises in return to deploy the contract and promises the contract to smoothly run the contract functions like mint and claim
 
   console.log("Crypto devs token address", deployCryptoDevsToken.address); // this shows the address of the contract crypto devs
